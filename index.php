c10fc3963265234937d5961c4d295921219fa1ed
Indro <indro.malleus@khk.ee>
2016-01-18 10:39:40 +0200

    Frontend

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<img src="logo.png" alt="" id="logo">
+<div id="kast" style="text-align:center;">
+<h1>Meliss broneering</h1>
+<p>Nimi</p>
+<input type="text">
+    <p>Telf nr:</p>
+    <input type="text">
+    <p>E-mail</p>
+    <input type="text">
+    <p>Inimeste arv</p>
+    <input type="text">
+    <p>Kuupäev ja kellaaeg</p>
+    <select name="aeg" id="aeg">
+        <option value="value">Vali kuupäev</option>
+        <option value="value">18.01.2016 11:00</option>
+        <option value="value">18.01.2016 12:00</option>
+        <option value="value">18.01.2016 13:00</option>
+    </select>
+    <br>
+    <br>
+    <br>
+<button class="submit">Sisesta</button>
+</div>
+</body>
+</html>
